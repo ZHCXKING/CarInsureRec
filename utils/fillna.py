18aec1310f97bf472b490431bc5721d72ddf1699
zhcxking <zhcxking@qq.com>
2025-12-09 13:36:55 +0800

    Add data loading utility

--- utils/fillna.py
+++ utils/fillna.py
@@ -6,19 +6,7 @@ from sklearn.impute import IterativeImputer
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import BayesianRidge
 #%%
-def iterative(df: pd.DataFrame, method: str='NB'):
-    df['year'] = df['Date'].dt.year
-    df['month'] = df['Date'].dt.month
-    df['day'] = df['Date'].dt.day
-    df.drop('Date', axis=1, inplace=True)
-    cols = df.columns
-    idx = df.index
-    if method == 'RF':
-        imputer = IterativeImputer(estimator=RandomForestRegressor(), max_iter=50, verbose=1)
-    elif method == 'NB':
-        imputer = IterativeImputer(estimator=BayesianRidge(), max_iter=50, verbose=1)
-    df = imputer.fit_transform(df)
-    df = pd.DataFrame(df, index=idx, columns=cols)
+def _round_and_Date(df:pd.DataFrame):
     discrete_cols = ['Age', 'DrivingExp', 'Occupation', 'NCD', 'InsCov', 'Make', 'Car.year', 'year', 'month', 'day']
     for col in discrete_cols:
         if col == 'NCD':
@@ -30,3 +18,41 @@ def iterative(df: pd.DataFrame, method: str='NB'):
     df.drop(['year', 'month', 'day'], axis=1, inplace=True)
     df.sort_values(by='Date', inplace=True, ignore_index=True)
     return df
+#%%
+def iterative(df: pd.DataFrame, method: str='NB'):
+    df['year'] = df['Date'].dt.year
+    df['month'] = df['Date'].dt.month
+    df['day'] = df['Date'].dt.day
+    df.drop('Date', axis=1, inplace=True)
+    cols = df.columns
+    idx = df.index
+    if method == 'RF':
+        imputer = IterativeImputer(estimator=RandomForestRegressor(), max_iter=50, verbose=1)
+    elif method == 'NB':
+        imputer = IterativeImputer(estimator=BayesianRidge(), max_iter=50, verbose=1)
+    else:
+        raise ValueError('method must be RF or NB')
+    df = imputer.fit_transform(df)
+    df = pd.DataFrame(df, index=idx, columns=cols)
+    df = _round_and_Date(df)
+    return df
+#%%
+def split_iterative(df: pd.DataFrame, split_num: int, method: str='NB'):
+    df['year'] = df['Date'].dt.year
+    df['month'] = df['Date'].dt.month
+    df['day'] = df['Date'].dt.day
+    df.drop('Date', axis=1, inplace=True)
+    cols = df.columns
+    if method == 'RF':
+        imputer = IterativeImputer(estimator=RandomForestRegressor(), max_iter=50, verbose=1)
+    elif method == 'NB':
+        imputer = IterativeImputer(estimator=BayesianRidge(), max_iter=50, verbose=1)
+    else:
+        raise ValueError('method must be RF or NB')
+    data_train = df[:split_num].reset_index(drop=True)
+    data_test = df[split_num:].reset_index(drop=True)
+    data_train = pd.DataFrame(imputer.fit_transform(data_train), columns=cols)
+    data_test = pd.DataFrame(imputer.transform(data_test), columns=cols)
+    data_train = _round_and_Date(data_train)
+    data_test = _round_and_Date(data_test)
+    return data_train, data_test
